7bd04487fcab49c6c2d928a924ef28fe400d3cad
iweinbau <iwein.bau@gmail.com>
2016-05-10 11:04:33 +0200

    Update scheduler and Task class

--- src/hillbillies/model/Scheduler.java
+++ src/hillbillies/model/Scheduler.java
@@ -1,7 +1,127 @@
 package hillbillies.model;
 
+import java.lang.reflect.GenericArrayType;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by joppegeluykens on 14/04/16.
  */
-public class Scheduler {
+public class Scheduler implements Iterator<Task> {
+	
+	private Faction faction;
+	
+	private List<Task> tasks;
+	
+	private HashMap<Task,Unit> assigned = new HashMap<Task, Unit>();
+
+	/**
+	 * Return the assigned of this Scheduler.
+	 */
+	public HashMap<Task, Unit> getAssigned() {
+		return assigned;
+	}
+
+	/**
+	 * Return the tasks of this Scheduler.
+	 */
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	/**
+	 * Set the tasks of this Scheduler to the given tasks.
+	 *
+	 * @param  tasks
+	 *         The tasks to set.
+	 * @post   The tasks of this of this Scheduler is equal to the given tasks.
+	 *       | new.gettasks() == tasks
+	 */
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	/**
+	 * Return the faction of this Scheduler.
+	 */
+	public Faction getFaction() {
+		return faction;
+	}
+
+	/**
+	 * Set the faction of this Scheduler to the given faction.
+	 *
+	 * @param  faction
+	 *         The faction to set.
+	 * @post   The faction of this of this Scheduler is equal to the given faction.
+	 *       | new.getfaction() == faction
+	 */
+	public void setFaction(Faction faction) {
+		this.faction = faction;
+	}
+	
+	public void add(Task task){
+		
+	}
+	
+	public void addAll(List<Task> task){
+		
+	}
+
+	public void remove() {
+		
+	}
+	
+	public void removeCollection(Collection<Task> tasks) {
+		
+	}
+	
+	public void replace(Task original, Task replacment){
+		
+	}
+	
+	public boolean hasAsTask(Task task){
+		return false;
+	}
+	
+	public boolean hasAsTasks(Collection<Task> task){
+		return false;
+	}
+	
+	public List<Task> getAll(){
+		return null;	
+	}
+	
+	public List<Task> getPositive(){
+		return null;
+	}
+	
+	public void markAssigned(Task task, Unit unit) {
+		
+	}
+	
+	public void resetAssigned(Task task, Unit unit) {
+		
+	}
+	/* (non-Javadoc)
+	 * @see java.util.Iterator#hasNext()
+	 */
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Iterator#next()
+	 */
+	@Override
+	public Task next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- src/hillbillies/model/Task.java
+++ src/hillbillies/model/Task.java
@@ -1,7 +1,89 @@
 package hillbillies.model;
 
+import hillbillies.model.statements.*;
+
 /**
  * Created by joppegeluykens on 14/04/16.
  */
 public class Task {
+	
+	/**
+	 * 
+	 *variable that's hold the name if this task
+	 * 
+	 */
+	private String taskName;
+	
+	
+	/**
+	 * 
+	 * variable that's hold the priority of this tasks 
+	 * 
+	 */
+	private int priority;
+	
+	/**
+	 * 
+	 * sequens of statements or a singel statement that contains the activities a unit must execute.
+	 * 
+	 */
+	private Statement activities;
+
+	/**
+	 * Return the activities of this Task.
+	 */
+	public Statement getActivities() {
+		return activities;
+	}
+
+	/**
+	 * Set the activities of this Task to the given activities.
+	 *
+	 * @param  activities
+	 *         The activities to set.
+	 * @post   The activities of this of this Task is equal to the given activities.
+	 *       | new.getactivities() == activities
+	 */
+	public void setActivities(Statement activities) {
+		this.activities = activities;
+	}
+
+	/**
+	 * Return the priority of this Task.
+	 */
+	public int getPriority() {
+		return priority;
+	}
+
+	/**
+	 * Set the priority of this Task to the given priority.
+	 *
+	 * @param  priority
+	 *         The priority to set.
+	 * @post   The priority of this of this Task is equal to the given priority.
+	 *       | new.getpriority() == priority
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	/**
+	 * Return the taskName of this Task.
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+
+	/**
+	 * Set the taskName of this Task to the given taskName.
+	 *
+	 * @param  taskName
+	 *         The taskName to set.
+	 * @post   The taskName of this of this Task is equal to the given taskName.
+	 *       | new.gettaskName() == taskName
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	
 }
