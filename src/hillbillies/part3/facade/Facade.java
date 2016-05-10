bef565e8ef092b414268d795d794c2d617b71ea2
Joppe Geluykens <joppe.geluykens@student.kuleuven.be>
2016-05-10 18:04:21 +0200

    Implement Facade

--- src/hillbillies/part3/facade/Facade.java
+++ src/hillbillies/part3/facade/Facade.java
@@ -23,56 +23,56 @@ public class Facade extends hillbillies.part2.facade.Facade implements IFacade {
 
     @Override
     public boolean isWellFormed(Task task) throws ModelException {
-        return false;
+        return task.isWellFormed();
     }
 
     @Override
     public Scheduler getScheduler(Faction faction) throws ModelException {
-        return null;
+        return new Scheduler(faction);
     }
 
     @Override
     public void schedule(Scheduler scheduler, Task task) throws ModelException {
-
+        scheduler.schedule(task);
     }
 
     @Override
     public void replace(Scheduler scheduler, Task original, Task replacement) throws ModelException {
-
+        scheduler.replace(original, replacement);
     }
 
     @Override
     public boolean areTasksPartOf(Scheduler scheduler, Collection<Task> tasks) throws ModelException {
-        return false;
+        return scheduler.hasAsTasks(tasks);
     }
 
     @Override
     public Iterator<Task> getAllTasksIterator(Scheduler scheduler) throws ModelException {
-        return null;
+        return scheduler.iterator();
     }
 
     @Override
     public Set<Scheduler> getSchedulersForTask(Task task) throws ModelException {
-        return null;
+        return task.getSchedulers();
     }
 
     @Override
     public Unit getAssignedUnit(Task task) throws ModelException {
-        return null;
+        return task.getAssignedUnit();
     }
 
     @Override
     public Task getAssignedTask(Unit unit) throws ModelException {
-        return null;
+        return unit.getAssignedTask();
     }
 
     @Override
     public String getName(Task task) throws ModelException {
-        return null;
+        return task.getName();
     }
 
     @Override
     public int getPriority(Task task) throws ModelException {
-        return 0;
+        return task.getPriority();
     }
 }
